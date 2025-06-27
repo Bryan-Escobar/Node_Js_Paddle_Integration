36b35a4a25796e78873854e11827aae95d30ab18
unknown <bryan.escobar2@catolica.edu.sv>
2025-06-27 11:33:32 -0600

    feat: subscription.actived feature already finished, finished subscription creation process

--- src/presentation/paddle-payments/PaddleController.ts
+++ src/presentation/paddle-payments/PaddleController.ts
@@ -20,7 +20,7 @@ export class PaddleController {
     //!! This is a requirement from Paddle to avoid retries
     res.status(200).send("Webhook received");
 
-    console.log(req.body.data);
+
 
 
 
@@ -30,9 +30,10 @@ export class PaddleController {
 
         console.log("Customer created with ID:", customer_id);
         const { name, email } = req.body.data;
+        //busca el usuario por email
         let user = await prisma.users.findFirst({
           where: {
-            paddle_customer_id: customer_id
+            email: email,
           }
         })
         if (!user) {
@@ -93,7 +94,7 @@ export class PaddleController {
           const transaction = await prisma.transactions.create({
             data: {
               user_id: user ? user.id : null,
-              transaction_id,
+              paddle_transaction_id: transaction_id,
               amount: grand_total,
               currency: currency_code,
               status,
@@ -106,6 +107,74 @@ export class PaddleController {
           console.log("Transaction created:", transaction);
 
         }
+        break;
+      case "subscription.activated":
+        {
+          console.log(req.body);
+          const { customer_id, status, next_billed_at, paused_at, canceled_at, created_at, updated_at } = req.body.data;
+          const { starts_at, ends_at } = req.body.data.current_billing_period;
+          const paddle_subscription_id = req.body.data.id;
+          const plan = req.body.data.items[0].price.name;
+
+
+
+          let user = await prisma.users.findFirst({
+            where: {
+              paddle_customer_id: customer_id
+            }
+          });
+          if (user) {
+
+            console.log("User found:", user);
+            const subscription = await prisma.subscriptions.create({
+              data: {
+                paddle_customer_id: customer_id,
+                user_id: user ? user.id : null,
+                paddle_subscription_id,
+                status,
+                plan,
+                next_bill_date: next_billed_at,
+                paused_at,
+                canceled_at,
+                created_at,
+                updated_at,
+                current_billing_period_start: starts_at,
+                current_billing_period_end: ends_at,
+              }
+            })
+
+
+            //add the subscription_id to the transaction made before
+            console.log("subscription id created:", subscription.id);
+            const transaction = await prisma.transactions.updateMany({
+              where: {
+                user_id: user.id,
+                status: "paid",
+                subscription_id: null, // Ensure we only update transactions without a subscription ID
+              },
+              data: {
+                subscription_id: subscription.id,
+              }
+            })
+            console.log("Customer ID:", customer_id);
+            console.log("Status:", status);
+            console.log("Next Billed At:", next_billed_at);
+            console.log("Paused At:", paused_at);
+            console.log("Canceled At:", canceled_at);
+            console.log("Created At:", created_at);
+            console.log("Updated At:", updated_at);
+            console.log("Starts At:", starts_at);
+            console.log("Ends At:", ends_at);
+            console.log("Paddle Subscription ID:", paddle_subscription_id);
+            console.log("Plan:", plan);
+
+          }
+          else {
+            console.log("User not found for customer ID:", customer_id);
+          }
+
+        }
+        break;
     }
 
   }
